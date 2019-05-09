a45fa395e85f1a55f74088fccebd133247cf0f2c
Cole Pedersen <colep@lendio.com>
2019-05-08 21:07:07 -0600

    udpate and delete methods

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -100,7 +100,23 @@ class ItemsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'text' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            $response = array('response' => $validator->messages(), 'success' => false);
+            return $response;
+        } else {
+            // Find an item
+            $item = Item::find($id);
+            // request input coming from the text field and so on
+            $item->text = $request->input('text');
+            $item->body = $request->input('body');
+            $item->save();
+
+            return response()->json($item);
+        }
     }
 
     /**
@@ -111,6 +127,10 @@ class ItemsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $item = Item::find($id);
+        $item->delete();
+
+        $response = array('response' => 'Item deleted', 'success' => true);
+        return $response;
     }
 }
